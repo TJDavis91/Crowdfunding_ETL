aaf3d38d0875c31b41b93a1528da0d34c055c18c
TJDavis91 <135285282+TJDavis91@users.noreply.github.com>
2023-09-20 17:22:20 -0500

    part 2 code

--- Resources/Crowdfunding_ERD.sql
+++ Resources/Crowdfunding_ERD.sql
@@ -12,34 +12,34 @@ drop table if exists Campaign cascade;
 CREATE TABLE "Campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" string   NOT NULL,
-    "description" string   NOT NULL,
+    "company_name" varchar(30)  NOT NULL,
+    "description" varchar(30)   NOT NULL,
     "goal" int   NOT NULL,
     "pledged" int   NOT NULL,
-    "outcome" string   NOT NULL,
+    "outcome" varchar(30)   NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" string   NOT NULL,
-    "currency" string   NOT NULL,
+    "country" varchar(30)   NOT NULL,
+    "currency" varchar(30)   NOT NULL,
     "launched_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" string   NOT NULL,
-    "subcategory_id" string   NOT NULL,
+    "category_id" varchar(30)   NOT NULL,
+    "subcategory_id" varchar(30)   NOT NULL,
     CONSTRAINT "pk_Campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "Category" (
-    "category_id" string   NOT NULL,
-    "category_name" string   NOT NULL,
+    "category_id" varchar(30)   NOT NULL,
+    "category_name" varchar(30)   NOT NULL,
     CONSTRAINT "pk_Category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "Subcategory" (
-    "subcategory_id" string   NOT NULL,
-    "subcategory" string   NOT NULL,
+    "subcategory_id" varchar(30)   NOT NULL,
+    "subcategory" varchar(30)   NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -47,9 +47,9 @@ CREATE TABLE "Subcategory" (
 
 CREATE TABLE "Contacts" (
     "contact_id" int   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "email" string   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "email" varchar(30)   NOT NULL,
     CONSTRAINT "pk_Contacts" PRIMARY KEY (
         "contact_id"
      )
@@ -63,4 +63,3 @@ REFERENCES "Category" ("category_id");
 
 ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "Subcategory" ("subcategory_id");
-
